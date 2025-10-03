189fd162ebe84ee25c5d35795d75df652021c46b
andresitungkirr <andresitungkir4857@gmail.com>
2025-10-03 14:41:12 +0700

    Andre
    Delllll

---
+++ T00.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.lang.Math;
+
+public class T00 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int x, y, hasilPenjumlahan, hasilPerkalian;
+
+        x = Integer.parseInt(input.nextLine());
+        y = Integer.parseInt(input.nextLine());
+        if (x % 2 == 0) {
+            System.out.println("Bilangan pertama: Genap");
+        } else {
+            System.out.println("Bilangan pertama: Ganjil");
+        }
+        if (y % 2 == 0) {
+            System.out.println("Bilangan kedua: Genap");
+        } else {
+            System.out.println("Bilangan kedua: Ganjil");
+        }
+        if (x > y) {
+            System.out.println("Bilangan pertama lebih besar");
+        } else {
+            if (y > x) {
+                System.out.println("Bilangan kedua lebih besar");
+            } else {
+                System.out.println("Kedua bilangan sama besar");
+            }
+        }
+        if (x % 2 == 0 && y % 2 == 0) {
+            hasilPenjumlahan = x + y;
+            System.out.println("Hasil penjumlahan: " + hasilPenjumlahan);
+        } else {
+            if (x % 2 != 0 && y % 2 != 0) {
+                hasilPerkalian = x * y;
+                System.out.println("Hasil perkalian: " + hasilPerkalian);
+            } else {
+                System.out.println("Berbeda jenis");
+            }
+        }
+    }
+}
